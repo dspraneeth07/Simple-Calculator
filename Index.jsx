03bda3a68c6df6f16d173b7a6b7344cee18c8ca6
SAI PRANEETH REDDY DHADI <dspraneeth07@gmail.com>
2024-09-01 20:03:08 +0530

    Add files via upload

---
+++ Index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+const Index = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+      <Calculator />
+    </div>
+  );
+};
+
+export default Index;
